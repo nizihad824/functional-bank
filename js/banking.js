376e3a87d3fc2e83792a349e5f394a1bb047cc05
nizihad824 <mdnurulislam97@gmail.com>
2022-02-14 18:06:23 +0100

    validation check

--- js/banking.js
+++ js/banking.js
@@ -36,7 +36,34 @@ function updateBalance(amount, isAdd) {
     }
 }
 
+document.getElementById('deposit-button').addEventListener('click', function () {
+    /* 
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText); */
 
+
+    // get and update deposit total
+    /*  
+    const depositTotal = document.getElementById('deposit-total');
+     const depositTotalText = depositTotal.innerText;
+     const previousDepositTotal = parseFloat(depositTotalText);
+ 
+     depositTotal.innerText = previousDepositTotal + depositAmount; */
+
+
+    // update balance
+    /* 
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount; */
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
+});
 
 
  
