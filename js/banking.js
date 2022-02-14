128c1c5026e59b06ab6f73d00003019bbe1274d8
nizihad824 <mdnurulislam97@gmail.com>
2022-02-14 17:57:37 +0100

    function getInput value

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,9 @@
-document.getElementById('deposit-button').addEventListener('click',function(){
-    console.log('clicked')
-    
-})
+function getInputValue(inputId) {
+    debugger;
+    const inputField = document.getElementById(inputId);
+    const inputAmountText = inputField.value;
+    const amountValue = parseFloat(inputAmountText);
+    // clear input field
+    inputField.value = '';
+    return amountValue;
+}
